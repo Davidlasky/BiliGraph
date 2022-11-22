5c20527147c7781b4e34fb9ab5cabcbe5f773dd9
Bruce Yin <tyin5@illinois.edu>
2022-11-22 22:26:12 +0000

    update node

--- src/node.h
+++ src/node.h
@@ -1,17 +1,19 @@
 #pragma once
 #include <string>
+#include <vector>
 using namespace std;
 class Node {
 public:
   string name;
-  long UID;
-  long views;
-  long likes;
-  long coins;
-  Node(string name, long UID, long views, long likes, long coins)
-      : name(name), UID(UID), views(views), likes(likes),
-        coins(coins) { /* nothing */
+  double views;
+  double likes;
+  double coins;
+  Node(vector<string> input)
+      : name(input[0]), views(stod(input[2])), likes(stod(input[3])),
+        coins(stod(input[4])) {
+    /*author exist in authorData.csv, read line by line*/
   }
-  Node() : name(""), UID(0), views(0), likes(0), coins(0) { /* nothing */
-  }
+  // Node() : name(""), views(0), likes(0), coins(0) {
+  /* author doesn't exist in authorData.csv, use -1 for views,likes and coins.
+   */
 };
