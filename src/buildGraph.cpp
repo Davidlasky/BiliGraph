f4862a714d058ce33376e7a92b5c2b5174f3350f
Bruce Yin <tyin5@illinois.edu>
2022-11-27 22:28:13 +0000

    read file node

--- src/buildGraph.cpp
+++ src/buildGraph.cpp
@@ -3,22 +3,38 @@
 
 using namespace std;
 
-buildGraph::buildGraph(const string &authorFile, const string &videoFile){
-    authorFile_ = authorFile;
-    videoFile_ = videoFile;
-    constructGraph();
+buildGraph::buildGraph(const string &authorFile, const string &videoFile) {
+  authorFile_ = authorFile;
+  videoFile_ = videoFile;
+  constructGraph();
 }
 
-void buildGraph::constructGraph(){
-    insertAuthor();
-    insertVideo();
+void buildGraph::constructGraph() {
+  insertAuthor();
+  insertVideo();
 }
 
-
-void buildGraph::insertAuthor(){
-
+void buildGraph::insertAuthor() {
+  std::ifstream text(authorFile_);
+  string lines;
+  while (getline(text, lines)) {
+    vector<string> line = SplitString(lines);
+    bGraph.insertNode(line);
+    int a = 0;
+    // flightMap_.insertNode(line);
+  }
 }
 
-void buildGraph::insertVideo(){
-
+vector<string> buildGraph::SplitString(string &str1) {
+  vector<string> fields;
+  std::string str = str1;
+  std::string::size_type pos;
+  while ((pos = str.find(",")) != std::string::npos) {
+    fields.push_back(str.substr(0, pos));
+    str.erase(0, pos + 1);
+  }
+  fields.push_back(str);
+  return fields;
 }
+
+void buildGraph::insertVideo() {}
--- src/buildGraph.h
+++ src/buildGraph.h
@@ -1,18 +1,24 @@
 #pragma once
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <set>
 #include <string>
+#include <utility>
+#include <vector>
 
 class buildGraph {
-    public:
-        buildGraph(const string &authorFile, const string &videoFile);
-    //call bGraph.insertNode and bGraph.insertEdge
-        void constructGraph();
-        void insertAuthor();
-        void insertVideo();
+public:
+  buildGraph(const string &authorFile, const string &videoFile);
+  // call bGraph.insertNode and bGraph.insertEdge
+  void constructGraph();
+  void insertAuthor();
+  void insertVideo();
 
-
-    private:
-        string authorFile_;
-        string videoFile_;
-        buildHelper bGraph;
-
+private:
+  string authorFile_;
+  string videoFile_;
+  buildHelper bGraph;
+  vector<string> buildGraph::SplitString(string &str1);
 };
