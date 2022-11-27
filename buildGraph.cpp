83c00cd79e5412946e7f643c23c2966f8ebafea1
SallyXue-0114 <92959551+SallyXue-0114@users.noreply.github.com>
2022-11-27 14:57:19 -0600

    basic buildGraph template

---
+++ buildGraph.cpp
@@ -0,0 +1,18 @@
+#include "buildGraph.h"
+
+using namespace std;
+
+void buildGraph::constructGraph(){
+    buildGraph bGraph;
+    bGraph.insertNode();
+    bGraph.insertEdge();
+}
+
+
+void buildGraph::insertNode(){
+
+}
+
+void buildGraph::insertEdge(){
+
+}
---
+++ buildGraph.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+class buildGraph {
+    public:
+
+    //call bGraph.insertNode and bGraph.insertEdge
+        void constructGraph();
+        void insertNode();
+        void insertEdge();
+
+
+    private:
+
+};
