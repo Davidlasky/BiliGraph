68e37885481f0dd22ad17ffc86533cc80933cf3b
Sally Xue <kexinx4@illinois.edu>
2022-11-27 23:23:40 +0000

    update buildGraph insertVideo - Sally

--- src/buildGraph.cpp
+++ src/buildGraph.cpp
@@ -35,4 +35,27 @@ vector<string> buildGraph::SplitString(string &str1) {
   return fields;
 }
 
-void buildGraph::insertVideo() {}
+
+
+void buildGraph::insertVideo() {
+  std::ifstream text(videoFile_);
+  string lines;
+  while (getline(text, lines)) {
+    vector<string> wholeline = SplitString(lines); 
+    vector<string> otherInfo;
+    for(unsigned int i = 0; i < wholeline.size()-1; i++){
+      otherInfo.push_back(wholeline.at(i));
+    }
+
+    string authorStr = wholeline.at(4);
+    string authorTrim = authorStr.substr(2, authorStr.size()-1);
+
+    vector<string> authorsStr = SplitString(authorTrim);
+    vector<int> authors;
+    for(unsigned int i = 0; i < authorsStr.size(); i++){
+      authors.push_back(stoi(authorsStr.at(i)));
+    }
+
+    bGraph.insertEdge(otherInfo, authors);
+  }
+}
