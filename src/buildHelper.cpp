ffa6f93eb4844e2dea66f5a72be3330b1f511763
Bruce Yin <tyin5@illinois.edu>
2022-11-28 00:23:37 +0000

    111

--- src/buildHelper.cpp
+++ src/buildHelper.cpp
@@ -9,19 +9,19 @@ void buildHelper::insertEdge(vector<string> otherInfo, vector<int> authors) {
   int source;
   int dest;
   vector<string> videos;
+  videos.push_back(otherInfo.at(0));
   vector<int> views;
+  views.push_back(std::stoi(otherInfo.at(1)));
   vector<int> likes;
+  likes.push_back(std::stoi(otherInfo.at(2)));
   vector<int> coins;
+  coins.push_back(std::stoi(otherInfo.at(3)));
 
   if (authors.size() > 1) { // have collaboration
     for (unsigned int i = 0; i < authors.size() - 1; i++) {
       for (unsigned int j = 1; j < authors.size(); j++) {
         source = authors.at(i);
         dest = authors.at(j);
-        videos.push_back(otherInfo.at(0));
-        views.push_back(std::stoi(otherInfo.at(1)));
-        likes.push_back(std::stoi(otherInfo.at(2)));
-        coins.push_back(std::stoi(otherInfo.at(3)));
 
         Edge e(source, dest, videos, views, likes, coins);
       }
